2dad358d3d2e7cdcd3b966eaa752b92364a02eaa
Shadrack <shadrackmeshack7@gmail.com>
2025-03-01 21:07:26 +0300

    python test

---
+++ app.js
@@ -0,0 +1,19 @@
+//DOM Manipulation
+const title = document.querySelector('.heading');
+title.style.color='red';
+console.log(title)
+
+// GetElementByClassName()
+
+
+//GetelementByTagName()
+
+
+
+
+// querySelector()
+
+
+
+
+// querySelectorAll()
