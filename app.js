dbc5aad828838c3920dbe35a1e27fff4508f46da
Shadrack <shadrackmeshack7@gmail.com>
2025-04-01 23:58:52 +0300

    updates

--- app.js
+++ app.js
@@ -12,4 +12,14 @@
     
 console.log(document.getElementsByTagName('ul')[0].innerHTML);
 
-console.log(document.querySelectorAll('li')[1-3].innerHTML);
+const listItems = document.querySelectorAll('li');
+for (let i = 0; i < listItems.length; i++) {
+    console.log(document.querySelectorAll('li')[i].innerHTML);
+}
+
+const ul=document.querySelector('ul')
+const li=document.querySelector('li')
+ul.append(li)
+// console.log(li)
+li.innerHTML='Hannah'
+// li.remove()[0]
