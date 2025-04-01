b82a87344ef6616653091ebfac69e290305f91e4
Shadrack <shadrackmeshack7@gmail.com>
2025-04-01 21:41:20 +0300

    updates

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //DOM Manipulation
 const title = document.querySelector('.heading');
 title.style.color='red';
-console.log(title)
+console.log(title);
 
+const h1=document.getElementsByTagName('h1');
+h1.style.color='red';
 // GetElementByClassName()
 
 
@@ -17,3 +19,14 @@ console.log(title)
 
 
 // querySelectorAll()
+
+const lu =document.getElementsByTagName('ul');
+const li = document.getElementsByClassName('list');
+
+lu.append(li);
+console.log(lu);
+li.innerhtml='gware'; 
+let a=2;
+let b =3;
+let c=a+b;
+console.log(c);
